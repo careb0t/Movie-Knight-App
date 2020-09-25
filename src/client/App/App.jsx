5e5140b575db4fe46e5388445cfa0b73ee17edab
Alex Bond <abond@fastvisa.us>
2020-09-25 08:46:25 -0500

    removed router link

--- src/client/App/App.jsx
+++ src/client/App/App.jsx
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 
 export default class App extends Component {
   render() {
     const App = () => (
       <div>
-        <Switch>
-          <Route exact path='/' component={Home}/>
-        </Switch>
+        <Home/>
       </div>
     )
     return (
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 render((
-    <Router>
-        <App/>
-    </Router>
+    <App/>
 ), document.getElementById('root'));
