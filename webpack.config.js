2dd44dd69d12df626bd078ee221d030f701b0693
Alex Bond <abond@fastvisa.us>
2020-09-25 08:50:04 -0500

    updated webpack publicpath

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, outputDirectory),
     filename: 'bundle.js',
-    publicPath: '/'
+    publicPath: '/Movie-Knight-App/'
   },
   module: {
     rules: [{
